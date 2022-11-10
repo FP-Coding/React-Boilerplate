aef06de49ac1c154501210fd5abc5aebd6f8f346
Felipe Pinto <felipe14062000@gmail.com>
2022-11-10 10:17:12 -0300

    feat: Adição e configuração da lib plop generator

---
+++ vite-project/generators/component.generator.cjs
@@ -0,0 +1,29 @@
+module.exports = {
+  description: 'application component',
+  prompts: [
+    {
+      type: "input",
+      name: "name",
+      message: 
+      "Qual o nome do componente? Caso o mesmo possua mais de uma palavra, separe as pondo um espaço entre elas!"
+    }
+  ],
+  actions: [
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/index.js',
+      templateFile: '.template/component/index.jsx.hbs'
+    },
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.jsx',
+      templateFile: './templates/component/component.jsx.hbs'
+    },
+    {
+      type: 'add',
+      path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.test.jsx',
+      templateFile: './templates/component/test.jsx.hbs'
+    }
+
+  ]
+}
