87670f74659fa7710a1eff34ce4e2b76f45ab750
Felipe Pinto <felipe14062000@gmail.com>
2022-12-03 20:24:07 -0300

    fix:
    configuração do  transforma para RTL funcionar

--- vite-project/jest.config.cjs
+++ vite-project/jest.config.cjs
@@ -5,6 +5,29 @@ module.exports = {
   modulePaths: ['<rootDir>/src/'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
   transform: {
-    '^.\\.jsx?$': 'babel-jest'
+    "^.+\\.(t|j)sx?$": [
+      "@swc/jest",
+      {
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+            decorators: true,
+          },
+          keepClassNames: true,
+          transform: {
+            legacyDecorator: true,
+            decoratorMetadata: true,
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+        module: {
+          type: "es6",
+          noInterop: false,
+        },
+      },
+    ],
   }
 }
